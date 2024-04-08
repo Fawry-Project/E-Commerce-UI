429c329ae03db3008d32a30ab9690a80f6f0c00b
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2024-04-08 19:16:46 +0200

    Add CreateCategory Form Componenet

--- e-commerce-ui/src/app/components/create-category/create-category.component.ts
+++ e-commerce-ui/src/app/components/create-category/create-category.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CategoryDTO } from '../../models/category-model';
 
 @Component({
   selector: 'app-create-category',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-category.component.html',
-  styleUrl: './create-category.component.css'
+  styleUrl: './create-category.component.css',
 })
 export class CreateCategoryComponent {
+  categoryName: string = '';
 
+  onSubmit(): void {
+    const categoryDTO: CategoryDTO = { categoryName: this.categoryName };
+    console.log(categoryDTO);
+    // Clear the input field after successful submission
+    this.categoryName = '';
+  }
 }
