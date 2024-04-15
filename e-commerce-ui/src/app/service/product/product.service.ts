d0d45a09411e139cbe495d12d1250dbdf9c8d087
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2024-04-15 02:33:43 +0200

    Add createProduct Method in ProductService

--- e-commerce-ui/src/app/service/product/product.service.ts
+++ e-commerce-ui/src/app/service/product/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { ProductDTO } from '../../models/product-model';
 
@@ -16,6 +16,28 @@ export class ProductService {
       catchError((error) => {
         console.error('Error fetching products:', error);
         return throwError(error);
-      }));
+      })
+    );
+  }
+
+  createProduct(product: any): Observable<any> {
+    return this.http
+      .post<any>(this.apiUrl, product)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // Server-side error
+      console.error(
+        `Backend returned code ${error.status}, ` +
+          `body was: ${JSON.stringify(error.error)}`
+      );
+    }
+    // Return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
   }
 }
